9ede18751e613976817d16957251982c65efa51e
uangwang <202119121729@stu.cdut.edu.cn>
2024-04-04 21:54:01 +0800

    分糖果修改

--- 算法训练/分糖果.py
+++ 算法训练/分糖果.py
@@ -1,22 +1,13 @@
-def find_2023(num):
-    one = str(num).find('2')
-    if  one == -1:
-        return False
-    two = str(num).find('0',one+1)
-    if  two == -1:
-        return False
-    three = str(num).find('2',two+1)
-    if  three == -1:
-        return False
-    four = str(num).find('3',three+1)
-    if  four == -1:
-        return False
-    
-    return True
-
-count = 0
-for i in range(12345678,98765433):
-    if find_2023(i) is False:
-        count += 1
-print(count)
-    
+ans = 0
+def dfs(depth, n, m):
+    global ans
+    if depth == 7:
+        if n == 0 and m == 0:
+            ans += 1
+        return
+    for i in range(n + 1):
+        for j in range(m + 1):
+            if 2 <= i + j <= 5 and i <= n and j <= m:
+                dfs(depth + 1, n - i, m - j)
+dfs(0,9,16)
+print(ans)
